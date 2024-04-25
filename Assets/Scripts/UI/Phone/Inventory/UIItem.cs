6ba511e9a308369768b3d4bf6f0a8623df713d6b
윤형빈 <wudqls98@gmail.com>
2024-04-25 21:55:01 +0900

    윤형빈 : 데이터베이스

--- Assets/Scripts/UI/Phone/Inventory/UIItem.cs
+++ Assets/Scripts/UI/Phone/Inventory/UIItem.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public enum ItemType
 {
+    Equipment,
     Weapon,
     Consumable,
     Goblin,
@@ -17,4 +18,11 @@ public class UIItem
     public ItemType itemType;
     public string itemName;
     public Sprite itemImage;
+
+    public bool Use()
+    {
+        return false;
+    }
+
+
 }
